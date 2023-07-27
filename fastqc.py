b95b668b73fae8324955e23102b55ad8e60da227
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-07-27 13:50:29 -0700

    Update fastqc.py

--- fastqc.py
+++ fastqc.py
@@ -5,14 +5,10 @@ def STEP1_Preprocessing(sample):
     os.system('fastqc -t 48 --nogroup '+sample+'_2.fq.gz')
 
 def Main():
-    #REF = 'mm10/' #'hg38/'
-    #SP = 'mm10' #'hg38'
-    #adapter = 'illumina' #'nextera'
-    PATH = './'
-    file_list=os.listdir(PATH)
+    file_list=os.listdir('./')
     f_list=[file for file in file_list if file.endswith("_1.fq.gz")]
     for fname in f_list:
         sample = fname.split('_1.fq.gz')[0]
-        print sample
+        print(sample)
         STEP1_Preprocessing(sample)
 Main()
