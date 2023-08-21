5be943029c29fe8d0309baa2161a809661c58936
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-08-21 12:58:25 -0700

    Update DepthOfCoverage.py

--- DepthOfCoverage.py
+++ DepthOfCoverage.py
@@ -1,18 +1,35 @@
-import string,sys,os
+import string,sys,os,subprocess
   
-path,co = './',0
-file_list = os.listdir(path)
-bam_list = sorted([file for file in file_list if file.endswith('.sort.bam')])
-for i in bam_list:
-    co += 1
-    sample = i.split('.sort.bam')[0]
-    print(co,i)
-    os.system('samtools depth '+sample+'.sort.bam > '+sample+'.sort.bam.depth')
+def step1():
+    path,co = './',0
+    file_list = os.listdir(path)
+    bam_list = sorted([file for file in file_list if file.endswith('.sort.bam')])
+    for i in bam_list:
+        co += 1
+        sample = i.split('.sort.bam')[0]
+        print(co,i)
+        os.system('samtools depth '+sample+'.sort.bam > '+sample+'.sort.bam.depth')
 
-    avg_depth_output = os.popen("awk '{sum+=$3} END {print \"Sample " + sample + " Average Depth of Coverage: \", sum/3080419480}' " + sample + ".sort.bam.depth").read().strip()
-    print(avg_depth_output)
+def step2():
+    path = './'
+    bam_files = sorted([file for file in os.listdir(path) if file.endswith('.sort.bam.depth')])
 
-    # Save the sample name and average depth into the output file
-    with open('Depth.out', 'a') as output_file:
-        output_file.write(avg_depth_output+'\n')
+    open('Depth.out2', 'w').close()
 
+    for i,depth_file in enumerate(bam_files):
+        sample = depth_file.split('.sort.bam.depth')[0]
+        print(f"Processing sample {i + 1}: {sample}")
+
+        wc_output = subprocess.check_output(['wc', '-l', depth_file]).decode().strip()
+        num_lines = int(wc_output.split()[0])
+
+        avg_depth_output = os.popen(f'awk \'{{sum+=$3}} END {{print "Average Depth of Coverage for {sample}: ", sum/{num_lines}}}\' '+ sample + '.sort.bam.depth').read().strip()
+        print(avg_depth_output)
+
+        with open('Depth.out2', 'a') as output_file:
+            output_file.write(avg_depth_output+'\n')
+
+def main():
+    #step1()
+    step2()
+main()
